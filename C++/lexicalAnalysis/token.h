58aa9b53fc6cd9a4c7c1d9f5b7475e2bebab8182
天知いのり <65227082+AmachiInori@users.noreply.github.com>
2020-08-15 01:30:57 +0800

    20200815

--- C++/lexicalAnalysis/token.h
+++ C++/lexicalAnalysis/token.h
@@ -2,50 +2,74 @@
 #include <iostream>
 #ifndef _TOKEN_H_
 #define _TOKEN_H_
-#define NUM 256
-#define ID 257
-#define TRUE 258
-#define FALSE 259
-#define FLOAT 260
-#define DOUOP 261
 
-using std::string;
+typedef short tokenType;
+static const tokenType _NUM = 0, _FLO = 1, _STR = 2, _CHA = 3;
+static const tokenType _EQUA = 4, _COMP = 5;
+static const tokenType _ID = 6, _RE = 7, _SP = 8;
 
 class token {
 public:
-    const int tag;
-    explicit token(int t = 0) : tag(t){};
-    virtual void print() const { cout << tag << '\t' << "TOKEN" << '\t' << (char)tag << endl; };
+    tokenType tags;
+    explicit token(tokenType _type) : tags(_type) {};
+    virtual void printToken() { std::cout << (int)tags; }
 };
-class num : public token { 
+
+class num : public token { //整形字面量
 public:
-    const int value;
-    explicit num(int v) : value(v), token(NUM){};
-    void print() const override { cout << tag << '\t' << "INTGE" << '\t' << value << endl; }
+    int value;
+    explicit num(int _number) : token(_NUM), value(_number) {}
+    void printToken() override { std::cout << "NUM " << value; }
 };
-class word : public token {
-public:
-    const string lexeme;
-    explicit word(int t, string S) : lexeme(S), token(t){};
-    void print() const override { cout << tag << '\t' << "WORDS" << '\t' << lexeme << endl; }
-};
-class floa : public token {
+class flo : public token { //浮点字面量
 public:
     float value;
-    floa(int first, int second) : token(FLOAT) {
-        float temp = second;
-        while(temp >= 1) temp /= 10;
-        temp += first;
-        value = temp;
-    }
-    void print() const override { cout << tag << '\t' << "FLOAT" << '\t' << value << endl; }
+    explicit flo(float _number) : token(_FLO), value(_number) {}
+    void printToken() override { std::cout << "FLO " << value; }
 };
-class doubleOp : public token {
+class str : public token { //字符串字面量
 public:
-    const char first;
-    const char second;
-    doubleOp(char fi, char sec) : first(fi), second(sec), token(DOUOP){};
-    void print() const override { cout << tag << '\t' << "DOUOP" << '\t' << first << second << endl; }
+    std::string value;
+    explicit str(std::string const &_val) : token(_STR), value(_val) {}
+    void printToken() override { std::cout << "STR " << value; }
+};
+class cha : public token { //字符字面量
+public:
+    char value;
+    explicit cha(char _val) : token(_CHA), value(_val) {}
+    void printToken() override { std::cout << "CHA " << value; }
+};
+
+class equa : public token { //赋值运算符 = += -=等
+public:
+    int type;
+    explicit equa(int _type) : token(_EQUA), type(_type) {}
+    void printToken() override { std::cout << "EQUA " << type; }
+};
+class comp : public token { //一般运算符
+public:
+    int type;
+    explicit comp(int _type) : token(_COMP), type(_type) {}
+    void printToken() override { std::cout << "COMP " << type; }
+};
+
+class id : public token { //一般标识符
+public:
+    std::string val;
+    explicit id(std::string const &_str) : token(_ID), val(_str) {}
+    void printToken() override { std::cout << "ID " << val; }
+};
+class re : public token { //保留字
+public:
+    int type;
+    explicit re(int _type) : token(_RE), type(_type) {}
+    void printToken() override { std::cout << "RE " << type; }
+};
+class sp : public token { //分隔符
+public:
+    char type;
+    explicit sp(char _type) : token(_SP), type(_type) {}
+    void printToken() override { std::cout << "SP " << type; }
 };
 
 #endif
--- C++/practice/tlsf.cpp
+++ C++/practice/tlsf.cpp
@@ -1,15 +1,14 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int unweightedPath (int totalNodeNumber, vector<vector <int>> Table, int start, int end) {
+int unweightedPath (int totalNodeNumber, vector<vector<int>> Table, int start, int end) {
     vector<int> distance(totalNodeNumber, int(-1));
     queue<int> BFS;
     BFS.push(start);
     distance[start] = 0;
     
-    int nowDist = 0;
-    int nowQueueSize = BFS.size();
-    while (nowQueueSize != 0) {
+    int nowQueueSize;
+    while (nowQueueSize = BFS.size()) {
         for (int i = 0; i < nowQueueSize; i++) {
             for (int j = 0; j < Table[BFS.front()].size(); j++) {
                 if (distance[Table[BFS.front()][j]] != -1) continue;
@@ -21,4 +20,43 @@ int unweightedPath (int totalNodeNumber, vector<vector <int>> Table, int start, 
         if (distance[end] != -1) return distance[end];
     }
     return distance[end];
+}
+
+int Dijkstra (int totalNodeNumber, vector<vector<pair<int, int>>> Table, int start, int end) {
+    vector<int> distance(totalNodeNumber, int(INT_MAX));
+    queue<int> BFS;
+    BFS.push(start);
+    distance[start] = 0;
+    
+    int nowQueueSize;
+    while (nowQueueSize = BFS.size()) {
+        for (int i = 0; i < nowQueueSize; i++) {
+            for (int j = 0; j < Table[BFS.front()].size(); j++) {
+                if (distance[Table[BFS.front()][j].first] > distance[BFS.front()] + Table[BFS.front()][j].second){
+                    BFS.push(Table[BFS.front()][j].first);
+                    distance[Table[BFS.front()][j].first] = distance[BFS.front()] + Table[BFS.front()][j].second;
+                }
+                
+            }
+            BFS.pop();
+        }
+    }
+    return distance[end];
+}
+
+
+int main() {
+    vector<vector<pair<int, int>>> test = {
+        {}, 
+        {{2, 10}, {4, 2}}, 
+        {{3, 5}}, 
+        {}, 
+        {{5, 3}}, 
+        {{2, 2}, {6, 1}, {7, 1}}, 
+        {{3, 4}}, 
+        {{8, 1}}, 
+        {{6, 1}}
+    };
+    cout << Dijkstra(9, test, 1, 3);
+    return 0;
 }
